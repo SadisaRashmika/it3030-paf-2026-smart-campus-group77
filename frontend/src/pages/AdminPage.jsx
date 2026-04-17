3107e52c9baede6794451aad27a4c2bcafcf0937
SadisaRashmika <sadisa.rashmika@gmail.com>
2026-04-17 21:53:29 +0530

    pages bug fixed.

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,44 +1,5 @@
-import { useOutletContext } from "react-router-dom";
-import PortalTabContent from "../member4-notifications-oauth/components/PortalTabContent";
+import RolePortalPage from "./RolePortalPage";
 
 export default function AdminPage() {
-  const {
-    user,
-    activeTab,
-    onLogin,
-    onNavigate,
-    adminUsers,
-    suspiciousUsers,
-    lecturerAssignments,
-    recoveryRequests,
-    loadingAdminData,
-    reloadAdminData,
-    onAssignLecturerWork,
-    onCreateStaffLogin,
-    onDeleteUser,
-    onDeactivateUser,
-    onApproveRecoveryRequest,
-    onRejectRecoveryRequest
-  } = useOutletContext();
-
-  return (
-    <PortalTabContent
-      tab={activeTab}
-      user={user}
-      onLogin={onLogin}
-      onNavigate={onNavigate}
-      adminUsers={adminUsers}
-      suspiciousUsers={suspiciousUsers}
-      lecturerAssignments={lecturerAssignments}
-      recoveryRequests={recoveryRequests}
-      loadingAdminData={loadingAdminData}
-      reloadAdminData={reloadAdminData}
-      onAssignLecturerWork={onAssignLecturerWork}
-      onCreateStaffLogin={onCreateStaffLogin}
-      onDeleteUser={onDeleteUser}
-      onDeactivateUser={onDeactivateUser}
-      onApproveRecoveryRequest={onApproveRecoveryRequest}
-      onRejectRecoveryRequest={onRejectRecoveryRequest}
-    />
-  );
+  return <RolePortalPage />;
 }
